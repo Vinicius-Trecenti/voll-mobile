1a0bde573e8714a4e0f937db5ec911eb96101a99
Vinicius Trecenti <vinitrecenti@gmail.com>
2024-05-27 16:47:11 -0300

    Add: checkbox to screen 3

--- src/Cadastro.tsx
+++ src/Cadastro.tsx
@@ -1,4 +1,4 @@
-import {VStack, Image, Text, Box, FormControl, Input, Button, Link} from 'native-base'
+import {VStack, Image, Text, Box, FormControl, Input, Button, Link, Checkbox} from 'native-base'
 import { TouchableOpacity } from 'react-native';
 import { Titulo } from './components/Titulo';
 import { Botao } from './components/Botao';
@@ -24,7 +24,8 @@ export default function Login() {
           label: 'Email',
           placeholder: 'Insira seu email...'
         }
-      ]
+      ],
+      checkbox: []
     },
     {
       id:2,
@@ -39,6 +40,22 @@ export default function Login() {
           id:2,
           label: 'Data de nascimento',
           placeholder: 'Insira sua data de nascimento...'
+        }
+      ],
+      checkbox: []
+    },
+    {
+      id:3,
+      titulo: 'Para finalizar, quais são o seus planos? ',
+      entradaTexto: [],	
+      checkbox: [
+        {
+          id: 1,
+          value: 'Sulamerica',
+        }, 
+        {
+          id: 2,
+          value: 'Unimed',
         }
       ]
     }
@@ -61,16 +78,23 @@ export default function Login() {
 
       <Titulo>{secoes[numSecao].titulo}</Titulo>
 
-      <Box>
-        
+      <Box>  
         {
-          secoes[numSecao].entradaTexto.map(entrada => {
+          secoes[numSecao]?.entradaTexto?.map(entrada => {
               return <EntradaTexto label={entrada.label} placeholder={entrada.placeholder} key={entrada.id} />
           })
         }
       </Box>
 
-    {numSecao > 0 && <Botao onPress={() => voltarSecao()} bgColor={'gray.400'}>Voltar</Botao>}
+      <Box>  
+        {
+          secoes[numSecao].checkbox.map(checkbox => {
+          return <Checkbox key={checkbox.id} value={checkbox.value}>{checkbox.value}</Checkbox>
+          })
+        } 
+      </Box>
+
+      { numSecao > 0 && <Botao onPress={() => voltarSecao()} bgColor={'gray.400'}>Voltar</Botao> }
       <Botao onPress={() => avancarSecao()} mt={4}>Avançar</Botao>
     </VStack>
   );
