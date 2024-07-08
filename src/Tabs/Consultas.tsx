89ab1675352e24de11149c67709c00f5a4fca13d
Vinicius Trecenti <vinitrecenti@gmail.com>
2024-07-08 19:34:40 -0300

    add component CardConsultas

--- src/Tabs/Consultas.tsx
+++ src/Tabs/Consultas.tsx
@@ -1,9 +1,15 @@
 import { VStack, Text } from "native-base"
+import { CardConsultas } from "../components/CardConsultas"
 
 export default function Consultas() { 
     return (
-        <VStack>
+        <VStack p="5">
             <Text>Consultas</Text>
+            <CardConsultas nome="Dr. Vinicius Trecenti" especialidade="Cardiologista"
+                foto="https://github.com/Vinicius-Trecenti.png"
+                data="08/07/2024"
+                //passar se foi atendido ou agendado
+                />
         </VStack>
     )
 }
---
+++ src/components/CardConsultas.tsx
@@ -0,0 +1,40 @@
+import { Text, Avatar, VStack } from 'native-base';
+import { Botao } from './Botao';
+
+interface CardProps {
+    nome: string;
+    foto: string;
+    especialidade: string;
+    data?: string; //significa que nao é obrigatório
+    foiAtendido?: boolean;
+    foiAgendado?: boolean;
+}
+
+export function CardConsultas({
+    nome,
+    foto,
+    especialidade,
+    data,
+    foiAtendido,
+    foiAgendado
+
+}: CardProps) {
+    return (
+        <VStack w="100%" p="5" bg={foiAtendido ? 'blue.100' : 'white'} borderRadius={"lg"} shadow={2}>
+
+            <VStack flexDir={"row"}>
+                <Avatar size="lg" source={{ uri: foto }} />
+                <VStack pl={4}>
+                    <Text fontSize={"md"} bold>{nome}</Text>
+                    <Text>{especialidade}</Text>
+                    <Text>{data}</Text>
+                </VStack>
+                
+            </VStack>
+
+            <Botao mt={4}>
+                {foiAgendado ? 'Cancelar consulta' : 'Agendar consulta'}
+            </Botao>
+        </VStack>
+    )
+}
